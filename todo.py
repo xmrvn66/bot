e818d7b27449547f2f490fb02fcfb693fd9b35d8
Marvin Kopietz <marvinkopietz@yahoo.de>
2021-03-30 21:37:17 +0200

    updated todo

--- todo.py
+++ todo.py
@@ -1,8 +1,17 @@
-# check for level ups or special event lootbox unlocked and navigate accordingly if they pop up
-# failsafes in case something pops up that bot doesn't know
-# navigate menue to map Dark Castle
-# log runtime + runs completed + monkeymoney looted + special currency looted
-# randomize every delay
-# randomize unnötige klicks -> Bspw. levelup.py (klicke irgendwo in Bereich zwischen x= 1000 - 1200 und Y = 900 - 950)
-# https://stackoverflow.com/questions/51825908/game-counter-in-python
-# if game /window with title "Bloons TD6" is not in Foreground, stop bot
+
+Not done Tasks:
+    # failsafes in case something pops up that bot doesn't know -> Any pop up
+    # navigate menue to map Dark Castle -> Naviagte Menue to choose Map depending on "Bonus available" -> Chose profile accordingly
+    # log runtime + runs completed + monkeymoney looted + special currency looted -> OCR Module Tesseract
+    # randomize every delay
+    # randomize unnötige klicks -> Bspw. levelup.py (klicke irgendwo in Bereich zwischen x= 1000 - 1200 und Y = 900 - 950)
+    # https://stackoverflow.com/questions/51825908/game-counter-in-python
+    # if game window with title "Bloons TD6" is not in Foreground, stop bot
+    # create functions for placing and upgrading Units -> place() -> upgradeUnit()
+
+
+
+"""
+Done Tasks:
+    # check for level ups or special event lootbox unlocked and navigate accordingly if they pop up
+"""
